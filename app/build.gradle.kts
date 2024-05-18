213833d9d3e0c19417e834b37ded844d1015df0d
Jacob <jacob.cube.mexx@gmail.com>
2024-05-18 13:10:15 +0200

    temporary PR skip 86byujgfd - dependencies, gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.gms.google.services)
 }
 
 android {
-    namespace = "lets.build.chatenricher"
+    namespace = "lets.build.chatenrichment"
     compileSdk = 34
 
+    packaging.run {
+        resources.excludes.add("META-INF/gradle/incremental.annotation.processors")
+    }
+
     defaultConfig {
-        applicationId = "lets.build.chatenricher"
+        applicationId = "lets.build.chatenrichment"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -21,12 +26,26 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+            isShrinkResources = false
+            applicationIdSuffix = ".test"
+        }
+        release {
+            // Enables code shrinking, obfuscation, and optimization for only
+            // your project's release build type.
+            isMinifyEnabled = true
+
+            // Enables resource shrinking, which is performed by the
+            // Android Gradle plugin.
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            signingConfig = signingConfigs["debug"]
         }
     }
     compileOptions {
@@ -50,20 +69,47 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.play.services)
+    //Credentials
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.auth)
+    implementation(libs.google.identity)
+
+    //Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material3.window.size)
+    implementation(libs.material.icons)
+    //Dagger, Hilt, Navigation, ViewModel
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compiler)
+    implementation(libs.kotlinx.metadata.jvm)
+    implementation(libs.androidx.hilt.navigation)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.viewmodel.compose)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+    implementation(libs.coil.svg)
+    implementation(libs.lottie)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
