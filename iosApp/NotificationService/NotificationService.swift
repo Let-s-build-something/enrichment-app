2642a5ec1236ec3b8ff6899c949a413183183efd
Jakub Kostka <jacob.cube.mexx@gmail.com>
2024-09-16 17:15:02 +0200

    refactor

--- iosApp/NotificationService/NotificationService.swift
+++ iosApp/NotificationService/NotificationService.swift
@@ -1,1 +1,79 @@
+import UserNotifications
+import FirebaseMessaging
+import ComposeApp
+
+class NotificationService: UNNotificationServiceExtension {
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        print("NotificationService, didReceive")
+        self.contentHandler = contentHandler
+        bestAttemptContent = HelperKt.onNewNotificationReceived(
+            request: request,
+            content: request.content.mutableCopy() as? UNMutableNotificationContent
+        )
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            
+            var urlString:String? = nil
+            let urlImageString = "https://miro.medium.com/v2/resize:fit:720/format:webp/1*2QjioLwVybwG1ToXxrOsOQ.png"
+            
+            if urlString != nil, let fileUrl = URL(string: urlString!) {
+                print("fileUrl: \(fileUrl)")
+                
+                guard let imageData = NSData(contentsOf: fileUrl) else {
+                    contentHandler(bestAttemptContent)
+                    return
+                }
+                guard let attachment = UNNotificationAttachment.saveImageToDisk(fileIdentifier: "image.jpg", data: imageData, options: nil) else {
+                    print("error in UNNotificationAttachment.saveImageToDisk()")
+                    contentHandler(bestAttemptContent)
+                    return
+                }
+                
+                bestAttemptContent.attachments = [ attachment ]
+            }
+            
+            //contentHandler(bestAttemptContent)
+            Messaging.serviceExtension().populateNotificationContent(
+                bestAttemptContent,
+                withContentHandler: contentHandler
+            )
+        }
+    }
+        
+    override func serviceExtensionTimeWillExpire() {
+        print("NotificationService, serviceExtensionTimeWillExpire")
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            bestAttemptContent.title = "a change"
+            bestAttemptContent.body = "another change"
+            contentHandler(bestAttemptContent)
+        }
+    }
+}
+
+extension UNNotificationAttachment {
+    
+    static func saveImageToDisk(fileIdentifier: String, data: NSData, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let folderName = ProcessInfo.processInfo.globallyUniqueString
+        let folderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(folderName, isDirectory: true)
+        
+        do {
+            try fileManager.createDirectory(at: folderURL!, withIntermediateDirectories: true, attributes: nil)
+            let fileURL = folderURL?.appendingPathComponent(fileIdentifier)
+            try data.write(to: fileURL!, options: [])
+            let attachment = try UNNotificationAttachment(identifier: fileIdentifier, url: fileURL!, options: options)
+            return attachment
+        } catch let error {
+            print("error \(error)")
+        }
+        
+        return nil
+    }
+}
 
