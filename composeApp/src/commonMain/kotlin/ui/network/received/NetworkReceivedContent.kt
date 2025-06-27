bad1ccd1e7fc337459553818b6fdc140794372b2
jacob <jakub.kostka@augmy.org>
2025-06-27 19:51:09 +0200

    refactor

--- composeApp/src/commonMain/kotlin/ui/network/received/NetworkReceivedContent.kt
+++ composeApp/src/commonMain/kotlin/ui/network/received/NetworkReceivedContent.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
